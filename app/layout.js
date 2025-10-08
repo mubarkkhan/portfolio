a2dfc8bdbad5aa84191108b18b4c4dbef0019ac7
mubarkkhan <ahad771418@gmail.com>
2025-10-09 01:30:53 +0530

    Initial commit

--- app/layout.js
+++ app/layout.js
@@ -84,7 +84,12 @@ export default function RootLayout({ children }) {
     }),
   }}
 />
-
+<Script
+      strategy="afterInteractive"
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2606365238590751"
+      crossorigin="anonymous"
+      async
+    />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
